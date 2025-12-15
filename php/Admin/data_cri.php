9e4b9c80eddaeb18b3768ff30fa009dcfc364db7
wan <zakwanzakki310806@gmail.com>
2025-12-15 14:36:29 +0700

    mengubah html warga menjadi php

--- php/Admin/data_cri.php
+++ php/Admin/data_cri.php
@@ -7,9 +7,7 @@
         
         <div class="input-group mb-3">
             <input type="text" class="form-control" placeholder="Masukkan NIK, Nama Warga, atau No. KK..." aria-label="Recipient's username" aria-describedby="button-addon2">
-            <button class="btn btn-primary" type="button" id="button-addon2">
-                <i class="bi bi-search"></i> Cari
-            </button>
+            <button class="btn btn-primary" type="button" id="button-addon2"><i class="bi bi-search"></i>Cari</button>
         </div>
 
         <div class="card">
--- php/Admin/header.php
+++ php/Admin/header.php
@@ -5,10 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aplikasi Administrasi Warga RT/RW</title>
-    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../css/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 
