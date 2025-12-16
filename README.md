12c20a1bb22ab57d96ec6f34977b069b20e11424
wan <zakwanzakki310806@gmail.com>
2025-12-16 23:07:31 +0700

    update file terbaru dari branch 1.0.1

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # PBL-Administrasi-Warga
 PBL Administrasi Warga RPL-101 C Semester 1
-
-- create new branch dulu jika ada yang mau edit
-- jangan langsung push ke main branch untuk meminimalisir error
-- inpokeun kalau ada perubahan yes
---
+++ php/Admin/sidebar.php
@@ -0,0 +1,8 @@
+    <div class="sidebar">
+        <a href="index.php"><i class="bi bi-speedometer2"></i> Dashboard</a>
+        <a href="data_cri.php"><i class="bi bi-search"></i> Pencarian Data</a>
+        <a href="data_wrg.php"><i class="bi bi-people-fill"></i> Data Warga</a>
+        <a href="data_klg.php" ><i class="bi bi-card-checklist"></i> Data Keluarga</a>
+        <a href="data_srt.php" ><i class="bi bi-envelope-fill"></i> Data Surat</a>
+        <a href="data_lpr.php" ><i class="bi bi-graph-up"></i> Laporan</a>
+    </div>
