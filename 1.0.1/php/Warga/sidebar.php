470ad20a284fba43384c27c29390cfaa6589ae78
wan <zakwanzakki310806@gmail.com>
2025-12-16 09:36:09 +0700

    update terbaru

--- 1.0.1/php/Warga/sidebar.php
+++
@@ -1,7 +0,0 @@
-    <div class="sidebar">
-        <a href="beranda.php" class="active"><i class="bi bi-speedometer2"></i> Beranda</a>
-        <a href="profil.php"><i class="bi bi-people-fill"></i> Profil Saya</a>
-        <a href="keluarga.php"><i class="bi bi-card-checklist"></i> Data Keluarga</a>
-        <a href="surat.php"><i class="bi bi-envelope-fill"></i> Data Surat</a>
-        <a href="pengumuman.php"><i class="bi bi-graph-up"></i> Pengumuman</a>
-    </div>
